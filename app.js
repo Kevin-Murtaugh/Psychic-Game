726d6919efc5f52c2a0b77849a5d554911aab0eb
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-03-29 07:37:37 -0400

    deleted code commented out & console.logs
    clean-looking code now

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var alphabet=["abcdefghijklmnopqrstuvwxyz"];
 alphabet= alphabet[0].split("");
 
 var guessArray=["          "];
-// alphabet= alphabet[0].split("");
 
 var wins= 0; 
 var losses = 0;
@@ -16,80 +15,40 @@ var reset = function() {
 
 var printIt = function() {
         document.getElementById("html").innerHTML =
-        "<p>Total Wins: " + wins + "</p>" +
-        "<p>Total Losses: " + losses + "</p>" +
-        "<p>Guesses so far: " + guessArray + "</p>" +
-        "<p>Remaining Guesses: " + guesses + "</p>";
+                "<p>Total Wins: " + wins + "</p>" +
+                "<p>Total Losses: " + losses + "</p>" +
+                "<p>Guesses so far: " + guessArray + "</p>" +
+                "<p>Remaining Guesses: " + guesses + "</p>";
 }
 
 var newletter =function(){
-randomLetter= alphabet[(Math.floor(Math.random()*alphabet.length))];
-console.log(randomLetter);}
+randomLetter= alphabet[(Math.floor(Math.random()*alphabet.length))];}
+
 
 newletter();
-console.log("one-time execution?")
-// fixit = "<p>Total Wins: " + wins + "</p>" +
-//         "<p>Total Losses: " + losses + "</p>" +
-//         "<p>Guesses so far: " + guessArray + "</p>" +
-//         "<p>Remaining Guesses: " + guesses + "</p>";
 
 document.onkeyup = function(event) {
     userGuess = event.key;
     guessArray[10-guesses]=userGuess;
     guesses--;
     printIt();
-//     document.getElementById("html").innerHTML =
-//                 "<p>Total Wins: " + wins + "</p>" +
-//                 "<p>Total Losses: " + losses + "</p>" +
-//                 "<p>Guesses so far: " + guessArray + "</p>" +
-//                 "<p>Remaining Guesses: " + guesses + "</p>";
-// //     fixit = "<p>Total Wins: " + wins + "</p>" +
-//             "<p>Total Losses: " + losses + "</p>" +
-//             "<p>Guesses so far: " + guessArray + "</p>" +
-//             "<p>Remaining Guesses: " + guesses + "</p>";   
 
 if (guesses <= 0){
-    console.log("guesses<=0");
     losses++;
     reset();
-//    guesses = 10;
-//    guessArray=["          "];
-        printIt();
-//     document.getElementById("html").innerHTML =
-//                 "<p>Total Wins: " + wins + "</p>" +
-//                 "<p>Total Losses: " + losses + "</p>" +
-//                 "<p>Guesses so far: " + guessArray + "</p>" +
-//                 "<p>Remaining Guesses: " + guesses + "</p>";
+    printIt();
     newletter();
-    console.log("increase losses",losses,"reset",guesses);
-    console.log("raw",wins, losses, guesses);
 }
 
     else if (randomLetter===userGuess){
         wins++;
         reset();
-//       guesses = 10; 
-//        guessArray=["          "];
         newletter();
-//        document.getElementById("html").innerHTML = fixit;
-        // document.getElementById("html").innerHTML =
-        //         "<p>Total Wins: " + wins + "</p>" +
-        //         "<p>Total Losses: " + losses + "</p>" +
-        //         "<p>Guesses so far: " + guessArray + "</p>" +
-        //         "<p>Remaining Guesses: " + guesses + "</p>";
         printIt();
-        console.log(html);
-        console.log("raw", wins, losses, guesses);
-        console.log("increase", wins, "reset", guesses);  
+
     }
         else {
-//        guesses--;
-//        guessArray[9-guesses]=userGuess;
-        // document.querySelector("#html").innerHTML = fixit;
- //       console.log(html);
- //       document.getElementById("html").innerHTML = fixit;
- //       console.log(html);
-        console.log("raw wins",wins,"losses", losses,"guesses", guesses);
- //       console.log("decrement guesses", guesses);
+// else do nothing except restart by obtaining a new guess
+
     }
 };
