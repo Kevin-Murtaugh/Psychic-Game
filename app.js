5e48ca86492d977e36cb606eed2cba503792147d
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-03-29 07:22:07 -0400

    added more outputs, ensure fixit is global
    It seems the logic was fine but the not "printing" enough to reflect each change as it happened;
    next need to convert some code to functions
    after that maybe add some CSS

--- app.js
+++ app.js
@@ -2,54 +2,76 @@ var alphabet=["abcdefghijklmnopqrstuvwxyz"];
 alphabet= alphabet[0].split("");
 
 var guessArray=["          "];
+// alphabet= alphabet[0].split("");
 
 var wins= 0; 
 var losses = 0;
 var guesses = 10;
 var randomLetter =null;
 
+var newletter =function(){
+randomLetter= alphabet[(Math.floor(Math.random()*alphabet.length))];
+console.log(randomLetter);}
 
+newletter();
+console.log("one-time execution?")
+fixit = "<p>Total Wins: " + wins + "</p>" +
+        "<p>Total Losses: " + losses + "</p>" +
+        "<p>Guesses so far: " + guessArray + "</p>" +
+        "<p>Remaining Guesses: " + guesses + "</p>";
 
-var newLetter =function(){
-randomLetter= alphabet[(Math.floor(Math.random()*alphabet.length))]
-console.log("newletter function: ", randomLetter);
-}
-
-newLetter();
 document.onkeyup = function(event) {
- //   prompt("Please enter your guess here:");
     userGuess = event.key;
     guessArray[10-guesses]=userGuess;
     guesses--;
-    console.log("inside",guesses, userGuess, guessArray);
-    
-    var fixit = "<p>Total Wins: " + wins + "</p>" +
-            "<p>Total Losses: " + losses + "</p>" +
-            "<p>Guesses so far: " + guessArray + "</p>" +
-            "<p>Remaining Guesses: " + guesses + "</p>";   
+    document.getElementById("html").innerHTML =
+                "<p>Total Wins: " + wins + "</p>" +
+                "<p>Total Losses: " + losses + "</p>" +
+                "<p>Guesses so far: " + guessArray + "</p>" +
+                "<p>Remaining Guesses: " + guesses + "</p>";
+//     fixit = "<p>Total Wins: " + wins + "</p>" +
+//             "<p>Total Losses: " + losses + "</p>" +
+//             "<p>Guesses so far: " + guessArray + "</p>" +
+//             "<p>Remaining Guesses: " + guesses + "</p>";   
 
 if (guesses <= 0){
+    console.log("guesses<=0");
     losses++;
     guesses = 10;
     guessArray=["          "];
-    console.log("guesses", guesses,"array", guessArray);
-    newLetter();
-    document.getElementById("html").innerHTML = fixit;
+    document.getElementById("html").innerHTML =
+                "<p>Total Wins: " + wins + "</p>" +
+                "<p>Total Losses: " + losses + "</p>" +
+                "<p>Guesses so far: " + guessArray + "</p>" +
+                "<p>Remaining Guesses: " + guesses + "</p>";
+    newletter();
+    console.log("increase losses",losses,"reset",guesses);
+    console.log("raw",wins, losses, guesses);
 }
 
     else if (randomLetter===userGuess){
         wins++;
         guesses = 10; 
         guessArray=["          "];
-        newLetter();
-        document.getElementById("html").innerHTML = fixit;
+        newletter();
+//        document.getElementById("html").innerHTML = fixit;
+        document.getElementById("html").innerHTML =
+                "<p>Total Wins: " + wins + "</p>" +
+                "<p>Total Losses: " + losses + "</p>" +
+                "<p>Guesses so far: " + guessArray + "</p>" +
+                "<p>Remaining Guesses: " + guesses + "</p>";
+        console.log(html);
+        console.log("raw", wins, losses, guesses);
+        console.log("increase", wins, "reset", guesses);  
     }
         else {
-
-        document.getElementById("html").innerHTML = fixit; 
-
-        // replace document.querySelector("#html").innerHTML = fixit;
-
-
+//        guesses--;
+//        guessArray[9-guesses]=userGuess;
+        // document.querySelector("#html").innerHTML = fixit;
+ //       console.log(html);
+ //       document.getElementById("html").innerHTML = fixit;
+ //       console.log(html);
+        console.log("raw wins",wins,"losses", losses,"guesses", guesses);
+ //       console.log("decrement guesses", guesses);
     }
 };
