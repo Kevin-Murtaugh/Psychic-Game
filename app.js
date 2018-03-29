2b2f3842c8ecd7a81fbb6abf6c5d8d3fa88667db
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-03-29 07:45:18 -0400

    added comments to Javascript file

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//psychic game javascript
+
+//declare & initialize all variables
 var alphabet=["abcdefghijklmnopqrstuvwxyz"];
 alphabet= alphabet[0].split("");
 
@@ -8,6 +11,7 @@ var losses = 0;
 var guesses = 10;
 var randomLetter =null;
 
+//reduce redundant code by creating functions
 var reset = function() {
         guesses =10;
         guessArray=["          "];
@@ -27,19 +31,23 @@ randomLetter= alphabet[(Math.floor(Math.random()*alphabet.length))];}
 
 newletter();
 
+//key press triggers the game
+//in the real world, this function would require extra steps to sanitize the code (i.e., make sure it' a letter, a lower-case letter, non-duplicate to earlier guesses, etc.
+//however, sine there was no such requirement in the directions, we will leave these enhancements for another day
 document.onkeyup = function(event) {
     userGuess = event.key;
     guessArray[10-guesses]=userGuess;
     guesses--;
     printIt();
 
+//if all guesses used, updates stats & restart
 if (guesses <= 0){
     losses++;
     reset();
     printIt();
     newletter();
 }
-
+// if match update stats, get a new random letter
     else if (randomLetter===userGuess){
         wins++;
         reset();
